de502708f735881dd0259d4fb269a0ffd637f135
Ioannis Karadimas <i.karadimas@pamediakopes.gr>
2013-10-12 01:08:36 +0300

    Orders sample listing + some basic layout.

--- coffee.js
+++ coffee.js
@@ -1,19 +1,53 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to coffee.";
-  };
+Orders = new Meteor.Collection('orders');
+Users = new Meteor.Collection('users');
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+if (Meteor.isClient) {
+    Template.orders.items = function () {
+        return Orders.find({ status: 'pending' });
+    };
+
+    Template.orders.events = {
+        'click .orders-list-entry': function (evt) {
+            var $this = $(evt.currentTarget);
+            if ($this.hasClass('active')) return;
+            $this.addClass('active');
+            evt.preventDefault();
+        }
+    };
+
+    Template.orders.rendered = function () {
+        $('.cnt').each(function () {
+            var $this = $(this);
+            var d = new Date($this.text());
+            $this.countdown({until: d, compact: true});
+        });
+    };
+
+    Meteor.startup(function () {
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+
+        function testOrders() {
+            if (Orders.findOne({vendor: 'Επίλεκτον'}) != null)
+                Orders.remove({vendor: 'Επίλεκτον'});
+
+            Orders.insert({
+                owner: 'Γιαννης Καραδήμας',
+                vendor: 'Επίλεκτον',
+                status: 'pending',
+                elapses: new Date(2013, 9, 14, 10, 30),
+                entries: [{
+                    quantity: 1,
+                    name: 'freddo espresso μέτριο με λίγο εβαπορέ',
+                    user: 'John'
+                }]
+            });
+        }
+
+        testOrders();
+    });
 }
